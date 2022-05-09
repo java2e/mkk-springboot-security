eb218a631fb72375660fa1484509b538205c3e84
Mehmet Demircioglu <demircioglumhmt@gmail.com>
2022-05-09 16:17:33 +0300

    Security Config edit

--- src/main/java/com/example/mkksecurity/security/WebSecurityConfig.java
+++ src/main/java/com/example/mkksecurity/security/WebSecurityConfig.java
@@ -1,23 +1,60 @@
 package com.example.mkksecurity.security;
 
+import com.example.mkksecurity.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+		prePostEnabled = true
+)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
+	@Autowired
 	private UserDetailsServiceImpl userDetailsService;
+
+
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+
+	@Autowired
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http.cors().and().csrf().disable()
+				.exceptionHandling().authenticationEntryPoint().and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.authorizeHttpRequests().antMatchers("/demo/**").permitAll()
+				.antMatchers("/api/test/**").permitAll()
+				.anyRequest().authenticated();
+
+		http.addFilterBefore(filteCustomize,Filter.class)
 
 	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 	}
 }
